de8fabc0f0f55a474627e7e6bd207e0246a2e9ce
Jonathan-home <joni_schaller@gmx.de>
2024-08-08 22:38:40 +0200

    server.js deleted openai stuff for now

--- server.js
+++ server.js
@@ -1,48 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-const OpenAI = require('openai');
 const cors = require('cors');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-
-app.get('/api/firebase-config', (req, res) => {
-    const firebaseConfig = {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID
-    };
-    res.json(firebaseConfig);
-});
-
-app.post('/api/chat', async (req, res) => {
-    const { conversationHistory } = req.body;
-    try {
-        const gptResponse = await openai.chat.completions.create({
-            model: 'gpt-4o',
-            messages: conversationHistory,
-            temperature: 1,
-            max_tokens: 256,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-        });
-        const reply = gptResponse.choices[0].message.content;
-        res.json({ reply });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'An error occurred while processing your request.' });
-    }
-});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
